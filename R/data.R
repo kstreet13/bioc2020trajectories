fa4aa0b4d23ffca23eba5fcc07c4d53c5286d6d6
Hector <hector.rouxdebezieux@berkeley.edu>
2020-06-17 11:43:01 +0200

    replaced data to rda

---
+++ R/data.R
@@ -0,0 +1,7 @@
+#' The object
+#'
+#' A \code{\link{SingleCellExperiment}} object of 9268 cells, derived from [McFaline-Figueroa, et al.](https://www.nature.com/articles/s41588-019-0489-5)
+#'
+#' @format Can be re-obtained by runing first chunk of the vignette.
+#' @usage data(sce)
+"sce"
